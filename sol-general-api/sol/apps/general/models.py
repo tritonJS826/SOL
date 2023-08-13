c32707a4d63464e862f7a24c9a3e887456947556
tritonJS826 <triton.68@mail.ru>
2023-08-14 00:56:09 +0400

    update db, added user, questionGroup, etc.

--- sol-general-api/sol/apps/general/models.py
+++ sol-general-api/sol/apps/general/models.py
@@ -1,14 +1,32 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+import uuid
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Profile(models.Model):
+    """Stores profile info"""
+
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.login}"
 
 
 class World(models.Model):
     """Stores world info"""
     
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    level_order_ids = ArrayField(models.UUIDField()) 
 
     def __str__(self):
         return f"{self.name}"
@@ -17,11 +35,13 @@ class World(models.Model):
 class Level(models.Model):
     """Stores level info"""
     
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
     world = models.ForeignKey(World, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    wave_order_ids = ArrayField(models.UUIDField()) 
 
     def __str__(self):
         return f"{self.title}"
@@ -30,30 +50,48 @@ class Level(models.Model):
 class Wave(models.Model):
     """Stores wave info"""
     
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     delay_time = models.PositiveIntegerField(validators=[MinValueValidator(1000), MaxValueValidator(86400000)])
     level = models.ForeignKey(Level, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    enemy_order_ids = ArrayField(models.UUIDField()) 
 
     def __str__(self):
-        return f"wave_{self.id}"
+        return f"wave_{self.uuid}"
+
+
+class QuestionGroup(models.Model):
+    """Stores question related data"""
+
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    question = models.TextField(null=False, blank=False)
+    answer = models.TextField(null=False, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"question_{self.uuid}"
 
 
 class Enemy(models.Model):
     """Stores enemy info"""
+    
     class EnemyType(models.TextChoices):
         """Enumerated enemy types."""
 
         CIRCLE = "CIRCLE", "Circle"
 
 
-    question = models.TextField(null=False, blank=False)
-    answer = models.TextField(null=False, blank=False)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    question_group = models.ForeignKey(QuestionGroup, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     enemy_type = models.CharField(max_length=255, null=False, blank=False, choices=EnemyType.choices) 
     waves = models.ManyToManyField(Wave, blank=True, related_name="enemies")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"enemy_{self.id}"
+        return f"enemy_{self.uuid}"
 
